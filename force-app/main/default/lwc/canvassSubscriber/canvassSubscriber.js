917e1ca810c31d72f2dd00738e19b823515a9665
MICHAEL ROBINSON <cheebinnsoo@gmail.com>
2020-11-19 13:22:14 +0000

    Canvass modified code

--- force-app/main/default/lwc/canvassSubscriber/canvassSubscriber.js
+++ force-app/main/default/lwc/canvassSubscriber/canvassSubscriber.js
@@ -1,25 +1,25 @@
 import { LightningElement,track,wire } from 'lwc';
-import {registerListener, unregisterAllListeners} from 'c/pubsub';
+import {registerListener,unregisterAllListeners} from 'c/pubsub';
 import {CurrentPageReference} from 'lightning/navigation';
 
-export default class CanvassSubscriber extends LightningElement {
-    @track color;
-    @wire(CurrentPageReference) pageRef;
+export default class CanvasSubscriber extends LightningElement {
+   @track color;
+   @wire(CurrentPageReference) pageRef;
 
-    connectedCallback(){
-        registerListener("changedColor")
-    }
-
-    disconnectedCallback(){
-        unregisterAllListeners(this);
-    }
-
-    handleChangedColor(colorCode){
-        console.log("color -->"+colorCode);
-        this.color=colorCode;
-    }
-
-    get colorSytle(){
-        return 'background-color:${this.color}';
+   connectedCallback()
+   {
+       registerListener("changedColor",this.handleChangedColor,this);
+   }
+   disconnectedCallback()
+   {
+       unregisterAllListeners(this);
+   }
+   handleChangedColor(colorCode)
+   {
+       console.log("Color -->"+colorCode);
+       this.color=colorCode;
+   }
+    get colorStyle(){
+        return `background-color:${this.color}`;
     }
 }
